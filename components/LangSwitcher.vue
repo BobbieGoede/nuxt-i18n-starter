5ba75460290c0c07b6dc7fee741e880fc500fa83
Bobbie Goede <bobbiegoede@gmail.com>
2024-11-15 13:00:32 +0100

    chore: setup starter for v9

--- components/LangSwitcher.vue
+++ components/LangSwitcher.vue
@@ -1,23 +1,24 @@
 <script lang="ts" setup>
 const { locales } = useI18n();
-const switchLocalePath = useSwitchLocalePath();
-
-const localeOptions = computed(() => locales.value.map((x) => (typeof x === "string" ? { code: x, name: x } : x)));
 </script>
 
 <template>
-	<div class="language-switcher">
-		<NuxtLink :to="switchLocalePath(locale.code)" v-for="locale in localeOptions" :key="locale.code">
-			{{ locale.name }}
-		</NuxtLink>
-	</div>
+  <div class="language-switcher">
+    <NuxtLink
+      :to="$switchLocalePath(locale.code)"
+      v-for="locale in locales"
+      :key="locale.code"
+    >
+      {{ locale.name }}
+    </NuxtLink>
+  </div>
 </template>
 
 <style>
 .language-switcher {
-	display: grid;
-	grid-auto-flow: column;
-	grid-auto-columns: max-content;
-	gap: 0.5em;
+  display: grid;
+  grid-auto-flow: column;
+  grid-auto-columns: max-content;
+  gap: 0.5em;
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,34 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	modules: ["@nuxtjs/i18n"],
-	devtools: { enabled: false },
-	i18n: {
-		lazy: true,
-		langDir: "locales",
-		defaultLocale: "en",
-		locales: [
-			{
-				name: "English",
-				code: "en",
-				iso: "en-US",
-				file: "en.json",
-			},
-			{
-				name: "Nederlands",
-				code: "nl",
-				iso: "nl-NL",
-				file: "nl.json",
-			},
-			{
-				name: "日本語",
-				code: "ja",
-				iso: "ja-JA",
-				file: "ja.json",
-			},
-		],
-		detectBrowserLanguage: false,
-	},
+  devtools: { enabled: true },
+
+  modules: ["@nuxtjs/i18n"],
+
+  i18n: {
+    lazy: true,
+    defaultLocale: "en",
+    locales: [
+      {
+        name: "English",
+        code: "en",
+        language: "en-US",
+        file: "en.json",
+      },
+      {
+        name: "Nederlands",
+        code: "nl",
+        language: "nl-NL",
+        file: "nl.json",
+      },
+      {
+        name: "日本語",
+        code: "ja",
+        language: "ja-JA",
+        file: "ja.json",
+      },
+    ],
+    detectBrowserLanguage: false,
+  },
+
+  compatibilityDate: "2024-11-15",
 });
